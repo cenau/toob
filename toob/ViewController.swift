548a7bebcca133a6f7c01c69dded87b01c80f0c4
Geraint Jones <geraintrjones@gmail.com>
2018-06-15 15:49:42 +0100

    Initial Commit

---
+++ toob/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  toob
+//
+//  Created by Geraint Jones on 15/06/2018.
+//  Copyright © 2018 Geraint Jones. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
